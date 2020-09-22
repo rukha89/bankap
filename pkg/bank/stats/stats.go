78ff81297003a318927c46bf4de7228cbc8978aa
Rukhshona <rukhshona_b@mail.ru>
2020-09-22 11:10:40 +0500

    add pkg

--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -5,11 +5,25 @@ import (
 )
 
 
+
+func Avg(payments []types.Payment) types.Money{
+	avg := types.Money(0)
+	sum := types.Money(0)
+	cnt := types.Money(0)
+	for _, payment := range payments {
+		sum += payment.Amount
+		cnt += types.Money(1)
+	}
+	avg = sum / cnt
+	return avg
+	
+}
+
 // TotalInCategory находит сумму покупок в определенной категории.
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
 	sum := types.Money(0)
 	for _, payment := range payments {
-		if payment.Category == category {
+		if payment.Category == category{
 			sum += payment.Amount
 		}
 	}
