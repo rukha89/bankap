cb0e7e96930a0403c3986871bfd2b2bfd3b13113
Rukhshona <rukhshona_b@mail.ru>
2020-09-22 11:07:21 +0500

    Добавление изменений

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -5,21 +5,6 @@ import (
 	"fmt"
 )
 
-func ExampleAvg() {
-	payments := []types.Payment{
-		{
-			Amount: 10_000_00, 
-		},
-		{
-			Amount: 5_000_00, 
-		},
-		{
-			Amount: 6_000_00, 
-		},
-	}
-	fmt.Println(Avg(payments))
-	// Output: 700000
-}
 
 
 func ExampleTotalInCategory() {
--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -4,18 +4,6 @@ import (
 	"github.com/coursar/bank/pkg/bank/types"
 )
 
-func Avg(payments []types.Payment) types.Money {
-	avg := types.Money(0)
-	sum := types.Money(0)
-	cnt := types.Money(0)
-	for _, payment := range payments {
-		sum += payment.Amount
-		cnt += types.Money(1)
-	}
-	avg = sum / cnt
-	return avg
-
-}
 
 // TotalInCategory находит сумму покупок в определенной категории.
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
