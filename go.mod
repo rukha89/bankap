f604d8ce5d78609ef799a88743b4e271ef81db21
Rukhshona <rukhshona_b@mail.ru>
2020-09-22 10:49:07 +0500

    инициализирован модуль

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module bank
+module github.com/coursar/bank
 
 go 1.15
-
-require github.com/coursar/bank v1.0.1
--- pkg/bank/types/types.go
+++ pkg/bank/types/types.go
@@ -5,7 +5,7 @@ type Money int64
 type Currency string
 
 //Коды валют
-const(
+const (
 	TJS Currency = "TJS"
 	USD Currency = "USD"
 	RUB Currency = "RUB"
@@ -14,24 +14,23 @@ const(
 type PAN string
 
 type Card struct {
-	ID			int
-	PAN			PAN
-	Balance		Money
-	Currency 	Currency
-	Color		string
-	Name		string
-	Active		bool
-	MinBalance	Money
-
+	ID         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+	MinBalance Money
 }
 
 type Payment struct {
-	ID		int
-	Amount	Money
+	ID     int
+	Amount Money
 }
 
 type PaymentSource struct {
-	Type string // 'card'
-	Number string // номер вида '5058 xxxx xxxx 8888'
-	Balance Money // баланс в дирамах
+	Type    string // 'card'
+	Number  string // номер вида '5058 xxxx xxxx 8888'
+	Balance Money  // баланс в дирамах
 }
