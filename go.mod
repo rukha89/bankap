2568678520b8cfc00d628de3e24cf5b8c5c71cbf
Rukhshona <rukhshona_b@mail.ru>
2020-09-22 10:34:35 +0500

    i dont know

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module bank
 
 go 1.15
+
+require github.com/coursar/bank v1.0.1
---
+++ pkg/bank/stats/example_test.go
@@ -0,0 +1,42 @@
+package stats
+
+import (
+	"github.com/coursar/bank/pkg/bank/types"
+	"fmt"
+)
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+			Amount: 10_000_00, 
+		},
+		{
+			Amount: 5_000_00, 
+		},
+		{
+			Amount: 6_000_00, 
+		},
+	}
+	fmt.Println(Avg(payments))
+	// Output: 700000
+}
+
+
+func ExampleTotalInCategory() {
+	payments := []types.Payment{
+		{
+			Amount: 10_000_00,
+			Category: `A`, 
+		},
+		{
+			Amount: 5_000_00, 
+			Category: `B`,
+		},
+		{
+			Amount: 6_000_00, 
+			Category: `A`,
+		},
+	}
+	fmt.Println(TotalInCategory(payments, `A`))
+	// Output: 1600000
+}
